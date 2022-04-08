2e2c4f5f1389c7d6cb68c9bb4886b047c1a5c36a
Boldi Bihari <boldibihari@gmail.com>
2022-04-08 09:47:34 +0200

    ViewModels created

---
+++ NationalChampionship/WPFClient/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using NationalChampionship.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace NationalChampionship.WPFClient.ViewModels
+{
+    public class MainWindowViewModel : ObservableRecipient
+    {
+        public RestCollection<Club> Clubs { get; set; }
+
+        public MainWindowViewModel()
+        {
+            Clubs = new RestCollection<Club>("https://localhost:44374/", "Club");
+        }
+    }
+}
